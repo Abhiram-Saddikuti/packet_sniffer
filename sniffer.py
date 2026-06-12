26a404016b7c40b92b7df0451ace34ca2c2bced3
Abhiram-Saddikuti <saddikutiatk@gmail.com>
2026-06-12 21:01:36 +0530

    Added port id's and a counter

--- sniffer.py
+++ sniffer.py
@@ -1,9 +1,27 @@
 from scapy.sendrecv import sniff
 from scapy.layers.inet import TCP, IP, UDP
 from scapy.layers.inet6 import IPv6
+packet_no = 1
 
+def get_service(port):
+
+    services = {
+        20: "FTP-DATA",
+        21: "FTP",
+        22: "SSH",
+        23: "TELNET",
+        25: "SMTP",
+        53: "DNS",
+        80: "HTTP",
+        110: "POP3",
+        143: "IMAP",
+        443: "HTTPS"
+    }
+
+    return services.get(port, "Unknown")
 
 def packet_callback(packet):
+    global packet_no
 
     if packet.haslayer(TCP):
 
@@ -23,9 +41,13 @@ def packet_callback(packet):
             print("Source IP       :", packet[IPv6].src)
             print("Destination IP  :", packet[IPv6].dst)
 
-        print("Source Port     :", packet[TCP].sport)
-        print("Destination Port:", packet[TCP].dport)
+        print("Source Port     :", packet[TCP].sport,
+              f"({get_service(packet[TCP].sport)})")
+        print("Destination Port:", packet[TCP].dport,
+              f"({get_service(packet[TCP].dport)})")
         print("Packet Length   :", len(packet), "bytes")
+        print(f"Packet No. : {packet_no}")
+        packet_no += 1
 
 
 
@@ -47,9 +69,13 @@ def packet_callback(packet):
             print("Source IP       :", packet[IPv6].src)
             print("Destination IP  :", packet[IPv6].dst)
 
-        print("Source Port     :", packet[UDP].sport)
-        print("Destination Port:", packet[UDP].dport)
+        print("Source Port     :", packet[UDP].sport,
+              f"({get_service(packet[UDP].sport)})")
+        print("Destination Port:", packet[UDP].dport,
+              f"({get_service(packet[UDP].dport)})")
         print("Packet Length   :", len(packet), "bytes")
+        print(f"Packet No. : {packet_no}")
+        packet_no += 1
 
 
 
