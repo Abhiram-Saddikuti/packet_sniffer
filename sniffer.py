6529a66e99733d303b744ff1a2aaa91f5b951589
Abhiram-Saddikuti <saddikutiatk@gmail.com>
2026-06-30 22:57:57 +0530

    Added an on-screen report with log generation for captures and alerts

--- sniffer.py
+++ sniffer.py
@@ -5,6 +5,8 @@ from scapy.layers.dns import DNS, DNSRR #for accessing DNS queries
 from scapy.layers.l2 import ARP #for accessing ARP layer
 from scapy.packet import Raw #for HTTP analysis
 from datetime import datetime #for timestamps
+import os #for log files
+
 
 BOLD = "\033[1m"  #changing output text colour
 GREEN = "\033[92m"
@@ -15,13 +17,35 @@ CYAN = "\033[96m"
 MAGENTA = "\033[95m"
 RESET = "\033[0m"
 
-
+session_time = datetime.now().strftime("%Y%m%d_%H%M%S")
 alerted_ips = set() #threat IPs for port scanning
 scan_tracker = {} #stores ports open for IPs
 handshakes = {} #stores S,SA,A packets for HANDSHAKES
 arp_table = {} #stores IP-MAC ARP mappings
 dns_table ={} #stores DNS responses (domain-address mappings)
-packet_no = 1 #packet counter
+
+#counters
+tcp_packets = 0
+udp_packets = 0
+dns_packets = 0
+arp_packets = 0
+
+http_requests = 0
+http_responses = 0
+
+handshake_count = 0
+
+portscan_alerts = 0
+arp_alerts = 0
+dns_alerts = 0
+
+start_time = datetime.now()
+
+os.makedirs("captures", exist_ok=True)
+os.makedirs("alerts", exist_ok=True)
+capture_log = f"captures/capture_{session_time}.log"
+alert_log = f"alerts/alerts_{session_time}.log"
+
 
 def get_service(port): #gets ports service name by number
 
@@ -49,9 +73,13 @@ def print_banner() :
 
 
 def handle_http(packet) :
+
     payload = packet[Raw].load.decode(errors="ignore")
     lines = payload.split("\r\n")
     if payload.startswith(("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")) :
+        global http_requests
+        http_requests += 1
+
         print(f"\n{MAGENTA}===================={RESET}")
         print(f"{MAGENTA}    HTTP REQUEST{RESET}")
         print(f"{MAGENTA}===================={RESET}")
@@ -64,8 +92,14 @@ def handle_http(packet) :
                 print(line)
             elif line.startswith("Accept:"):
                 print(line)
+        
+        log_capture(
+            f"HTTP REQUEST | {lines[0]}"
+                )
     
     elif payload.startswith("HTTP/") :
+        global http_responses
+        http_responses += 1
         print(f"\n{MAGENTA}===================={RESET}")
         print(f"{MAGENTA}   HTTP RESPONSE{RESET}")
         print(f"{MAGENTA}===================={RESET}")
@@ -78,16 +112,18 @@ def handle_http(packet) :
                 print(line)
             if line.startswith("Content-Length:"):
                 print(line)
-
+        log_capture(
+            f"HTTP RESPONSE | {lines[0]}"
+                )
 
 
 #UDP Block
 def handle_udp(packet) :
-    global packet_no
+    
+    global udp_packets
+    udp_packets += 1
     print(f"\n{YELLOW}[{datetime.now().strftime('%H:%M:%S')}]{RESET}")
-    print(f"\nPacket No. : {packet_no}")
-    packet_no += 1
-
+    
     print(f"\n{GREEN}===================={RESET}")
     print(f"     {GREEN}UDP Packet{RESET}")
     print(f"{GREEN}===================={RESET}")
@@ -97,18 +133,26 @@ def handle_udp(packet) :
         print("IP Version      : IPv4")
         print("Source IP       :", packet[IP].src)
         print("Destination IP  :", packet[IP].dst)
+        src_ip = packet[IP].src
+        dest_ip = packet[IP].dst
 
     elif packet.haslayer(IPv6): #IPv6 layer
 
         print("IP Version      : IPv6")
         print("Source IP       :", packet[IPv6].src)
         print("Destination IP  :", packet[IPv6].dst)
+        src_ip = packet[IPv6].src
+        dest_ip = packet[IPv6].dst
 
     print("Source Port     :", packet[UDP].sport, #port number
             f"({get_service(packet[UDP].sport)})") #port name
     print("Destination Port:", packet[UDP].dport,
             f"({get_service(packet[UDP].dport)})")
     print("Packet Length   :", len(packet), "bytes") #packet length
+
+    log_capture(
+        f"UDP | {src_ip}:{packet[UDP].sport} -> {dest_ip}:{packet[UDP].dport}"
+    )
 
 
 def get_dns_record_type(record_type) :
@@ -122,10 +166,10 @@ def get_dns_record_type(record_type) :
 
 #DNS Block
 def handle_dns(packet) :
-    global packet_no
+
+    global dns_packets
+    dns_packets += 1
     print(f"\n{YELLOW}[{datetime.now().strftime('%H:%M:%S')}]{RESET}")
-    print(f"\nPacket No. : {packet_no}")
-    packet_no += 1
 
     print(f"\n{BLUE}===================={RESET}")
     if packet[DNS].qr == 0 : #query
@@ -147,6 +191,9 @@ def handle_dns(packet) :
                 print(f"Domain      : {domain}")
                 print(f"Record Type : {record_type}")
                 print(f"IP Address  : {ip}")
+                log_capture(
+                    f"DNS | {domain} -> {ip} ({record_type})"
+                    )
             
             else :
                 if dns_table[key] == ip :
@@ -155,20 +202,24 @@ def handle_dns(packet) :
                     print(f"Record Type : {record_type}")
                     print(f"IP Address  : {ip}")
                 else : 
+                    global dns_alerts
+                    dns_alerts += 1
                     print(f"\n{YELLOW}[DNS] Possible DNS Poisoning Detected{RESET}")
                     print(f"Domain : {key}")
                     print(f"Original IP : {dns_table[key]}")
                     print(f"New IP : {ip}")
+                    log_alert(
+                        f"DNS POISONING | Domain={key} | Old={dns_table[key]} | New={ip}"
+                        )
 
 
     
-
 #ARP Block
 def handle_arp(packet) :
-    global packet_no
+
+    global arp_packets
+    arp_packets += 1
     print(f"\n{YELLOW}[{datetime.now().strftime('%H:%M:%S')}]{RESET}")
-    print(f"\nPacket No. : {packet_no}")
-    packet_no += 1
 
     if packet[ARP].op == 2 : #Only looks at ARP replys
         print(f"\n{CYAN}===================={RESET}")
@@ -188,21 +239,29 @@ def handle_arp(packet) :
 
         else : #IP already present
             if arp_table[ip] != mac : #different MAC add.
+                global arp_alerts
+                arp_alerts += 1
                 print(f"\n{RED}[ALERT] POSSIBLE ARP SPOOFING{RESET}")
                 print("IP Address : ", ip)
                 print("Original MAC : ", arp_table[ip])
                 print("New MAC : ", mac)
+                log_alert(
+                    f"ARP SPOOF | IP={ip} | Original={arp_table[ip]} | New={mac}"
+                    )
             else : #same MAC add.
                 print(f"{GREEN}[ARP]{RESET} Verified")
                 print(f"IP Address  : {ip}")
                 print(f"MAC Address : {mac}")
+        log_capture(
+            f"ARP | {ip} -> {mac}"
+            )
 
 #TCP Block
 def handle_tcp(packet) :
-    global packet_no
+
+    global tcp_packets
+    tcp_packets += 1
     print(f"\n{YELLOW}[{datetime.now().strftime('%H:%M:%S')}]{RESET}")
-    print(f"\nPacket No. : {packet_no}")
-    packet_no += 1
 
     print(f"\n{MAGENTA}===================={RESET}")
     print(f"{MAGENTA}     TCP Packet{RESET}")
@@ -247,10 +306,15 @@ def handle_tcp(packet) :
         if (len(scan_tracker[src_ip]) >= 5 #threshold for PORT SCAN
             and src_ip not in alerted_ips
             ):
+            global portscan_alerts
+            portscan_alerts += 1
             print(f"{RED}[ALERT] POSSIBLE PORT SCAN DETECTED{RESET}")
             print("Source IP :",src_ip)
             print("Ports : ",sorted(scan_tracker[src_ip]))
             alerted_ips.add(src_ip)
+            log_alert(
+                f"PORT SCAN | Source={src_ip} | Ports={sorted(scan_tracker[src_ip])}"
+                    )
 
 
     elif flags == "SA" : #SYN ACK packets
@@ -267,14 +331,24 @@ def handle_tcp(packet) :
                 print("Client : ",src_ip)
                 print("Server : ",dest_ip)
                 del handshakes[connection] #deletes the current connection IPs
+                global handshake_count
+                handshake_count += 1
+                log_capture(
+                    f"HANDSHAKE | {src_ip} <-> {dest_ip}"
+                    )
 
     if packet.haslayer(Raw) :
         handle_http(packet)
 
+    log_capture(
+        f"TCP | {src_ip}:{packet[TCP].sport} -> {dest_ip}:{packet[TCP].dport}"
+    )
+
+    
+
 
 
 def packet_callback(packet): #reads packets
-    global packet_no
 
     if packet.haslayer(DNS): #DNS packets
         handle_dns(packet)
@@ -288,5 +362,92 @@ def packet_callback(packet): #reads packets
     elif packet.haslayer(UDP): #UDP packets
         handle_udp(packet)
 
+
+def print_summary():
+    global tcp_packets, arp_packets, dns_packets, udp_packets
+    total_packets = tcp_packets + arp_packets + dns_packets + udp_packets
+    runtime = datetime.now() - start_time
+    total_seconds = int(runtime.total_seconds())
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+    seconds = total_seconds % 60
+
+    print(f"\n{CYAN}=========================================={RESET}")
+    print(f"{CYAN}          CAPTURE SUMMARY{RESET}")
+    print(f"{CYAN}=========================================={RESET}")
+
+    if hours > 0 :
+        runtime_string = f"{hours} hr {minutes} min {seconds} sec"
+    elif minutes >0 :
+        runtime_string = f"{minutes} min {seconds} sec"
+    else :
+        runtime_string = f"{seconds} sec"
+    print(f"Runtime             : {runtime_string}")
+
+    print(f"\nPackets Captured    : {total_packets}")
+
+    print("\nProtocol Statistics")
+    print("-------------------")
+    print(f"TCP Packets         : {tcp_packets}")
+    print(f"UDP Packets         : {udp_packets}")
+    print(f"DNS Packets         : {dns_packets}")
+    print(f"ARP Packets         : {arp_packets}")
+
+    print("\nHTTP Statistics")
+    print("-------------------")
+    print(f"HTTP Requests       : {http_requests}")
+    print(f"HTTP Responses      : {http_responses}")
+
+    print("\nSecurity Statistics")
+    print("-------------------")
+    print(f"TCP Handshakes      : {handshake_count}")
+    print(f"Port Scan Alerts    : {portscan_alerts}")
+    print(f"ARP Alerts          : {arp_alerts}")
+    print(f"DNS Alerts          : {dns_alerts}")
+
+    print("\nTables")
+    print("-------------------")
+    print(f"Known Devices       : {len(arp_table)}")
+    print(f"Known Domains       : {len(dns_table)}")
+
+    print(f"{CYAN}=========================================={RESET}")
+
+
+def log_capture(message) :
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open(capture_log, "a") as file:
+        file.write(f"[{timestamp}] {message}\n")
+def log_alert(message):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open(alert_log, "a") as file:
+        file.write(f"[{timestamp}] {message}\n")
+
+log_capture("=" * 60)
+log_capture("Packet Sniffer Capture Started")
+log_capture(f"Session Time : {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+log_capture("=" * 60)
+
+log_alert("=" * 60)
+log_alert("Packet Sniffer Alert Log Started")
+log_alert(f"Session Time : {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+log_alert("=" * 60)
+
 print_banner()
-sniff(prn=packet_callback, store=False) #starts sniffing packets until terminated
+
+
+try :   
+    sniff(prn=packet_callback, store=False) #starts sniffing packets until terminated
+finally :
+    print_summary()
+    
+    log_capture("=" * 60)
+    log_capture("Packet Sniffer Capture Finished")
+    log_capture(f"Session End : {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    log_capture("=" * 60)
+
+    log_alert("=" * 60)
+    log_alert("Packet Sniffer Alert Log Finished")
+    log_alert(f"Session End : {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    log_alert("=" * 60)
+
+    print(f"\n{GREEN}Capture Finished!{RESET}")
